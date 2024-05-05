8b28b70050dc8a356d04dfe6d613207cf2c01598
allen <g01.webapp@gmail.com>
2024-05-05 08:58:16 +0800

    修改 app.vue, 改用select, 簡化版面

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <script setup>
-import { RouterLink, RouterView} from 'vue-router'
+import { RouterLink, RouterView, useRouter, useRoute } from 'vue-router'
+const days = 2;
+
+const router = useRouter()
+
+// 取得目前的路由
+const route = useRoute()
+
+const isMathPath = (day) => {
+  return route.path.includes(formatRouterPath(day))
+}
+
+const formatRouterPath = (day) => {
+  return `d${String(day).padStart(2, '0')}`
+}
+
+const formatDay = (day) => {
+  return `day ${String(day).padStart(2, '0')}`
+}
+
+const handleChange = (e) => { 
+  const path = e.target.value
+  console.info("selected path", path);
+  router.push(path)
+}
 </script>
 
 <template>
   <nav>
-    <ul>
+    <!-- <ul>
       <li><router-link to="/d01">Day01</router-link></li>
       <li><router-link to="/d02">Day02</router-link></li>
-    </ul>
+    </ul> -->
+    goto : <select @change="handleChange">
+      <option 
+        v-for="day in days" 
+        :value="formatRouterPath(day)" 
+        :selected="isMathPath(day)">{{ formatDay(day) }}</option>
+    </select>
   </nav>
   <div>
     <router-view></router-view>
   </div>
+
+  
 </template>
 
 <style scoped>
